3de47061baf70405d6091f52fa87a2fe2f03156b
vanshbaranwal <vanshveerbaranwal@gmail.com>
2025-07-10 23:54:55 +0530

    added add task feature

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// const addTaskBtn = document.getElementById("add-task-btn")
+const addTaskBtn = document.getElementById("add-task-btn")
+const todoBoard = document.getElementById("todo-board")
 
-// addTaskBtn.addEventListener('click', () => {
-//     const input = prompt('what is the task??');
-// })
 
-alert("vansh baranwal")
+addTaskBtn.addEventListener('click', () => {
+    const input = prompt('what is the task??');
+    if(!input) return
+
+    const taskCard = document.createElement('p')
+    taskCard.classList.add('item')
+    taskCard.innerText = input
+
+    todoBoard.appendChild(taskCard);
+})
+
