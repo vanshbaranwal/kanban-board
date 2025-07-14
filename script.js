105f20eb06f00019f8d618a254bc2e0312ebc31a
vanshbaranwal <vanshveerbaranwal@gmail.com>
2025-07-14 21:18:47 +0530

    added drag and drop functionality

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const addtaskbutton = document.getElementById("add-task-button");
 const todoboard = document.getElementById("todo-board");
 
+function attachdragevents(target){
+    target.addEventListener('dragstart', () => {
+        target.classList.add('flying');
+    });
+    target.addEventListener('dragend', () => {
+        target.classList.remove('flying');
+    });
+}
 
 addtaskbutton.addEventListener('click', () => {
     const input = prompt("what is the task??");
@@ -10,6 +18,21 @@ addtaskbutton.addEventListener('click', () => {
     taskcard.classList.add("item");
     taskcard.setAttribute("draggable", true)
     taskcard.innerText = input;
+    attachdragevents(taskcard);
     todoboard.appendChild(taskcard);
 
 })
+
+const allboards = document.querySelectorAll(".board")
+const allitems = document.querySelectorAll(".item");
+
+allitems.forEach((item) => attachdragevents(item));
+
+allboards.forEach((board) => {
+    board.addEventListener('dragover', () => {
+        const flyingelemnt = document.querySelector('.flying');
+        console.log(board,"kuch toh upar se gya");
+
+        board.appendChild(flyingelemnt);
+    });
+});
